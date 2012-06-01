578032455e8c8712cc9e68be3a4edb5f1b05614b
Jorge Calás Lozano <calas@qvitta.net>
2012-06-01 02:27:48 +0200

    Update Rdoc documentation

--- lib/zimbra_wsdl.rb
+++ lib/zimbra_wsdl.rb
@@ -5,8 +5,12 @@ module ZimbraWsdl
   #
   # Example:
   #
+  #   require 'savon'
+  #   require 'zimbra_wsdl'
+  #
   #   client = Savon::Client.new do
   #     wsdl.document = ZimbraWsdl.admin_wsdl
+  #     wsdl.endpoint = 'https://zimbra.example.com:7071/service/admin/soap'
   #   end
   #
   def self.admin_wsdl
